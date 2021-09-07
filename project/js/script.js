b14c84147f3640f3296d7b7d0904f74482791c94
Linus Romland <romlandlinus@gmail.com>
2021-09-07 17:35:34 +0200

    Started on landing page design
    Co-Authored-By: Markus Simonsen <33492046+Markussim@users.noreply.github.com>
    Co-Authored-By: Svante Jonsson <77492340+svante-jonsson@users.noreply.github.com>

--- project/js/script.js
+++ project/js/script.js
@@ -1,6 +1,7 @@
 document.getElementById("burgerButton").onclick = () => {
-    let navItems = document.getElementById("navItems")
-    if (navItems.style.visibility == "hidden") {
+    let navItems = document.getElementById("linkContainer")
+
+    if (navItems.style.visibility == "hidden" || navItems.style.visibility == "") {
         navItems.style.visibility = "visible"
     } else {
         navItems.style.visibility = "hidden"
