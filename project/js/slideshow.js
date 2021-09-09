9871cb5c4916e449c509fa3d809919ffd8f62ec8
Linus Romland <romlandlinus@gmail.com>
2021-09-09 13:53:43 +0200

    format and fix

--- project/js/slideshow.js
+++ project/js/slideshow.js
@@ -2,7 +2,9 @@ let slideshows = document.getElementsByClassName("slideshow")
 
 for (let i = 0; i < slideshows.length; i++) {
     let slideshow = slideshows[i]
-    setInterval( function() { showSlides(slideshow) }, 3500 );
+    setInterval(function () {
+        showSlides(slideshow)
+    }, 3000 + Math.floor(Math.random() * 1000));
 }
 
 function showSlides(slideshow) {
@@ -10,10 +12,10 @@ function showSlides(slideshow) {
     let id;
 
     for (let i = 0; i < slides.length; i++) {
-        if(slides[i].classList.contains("active")) id = i
+        if (slides[i].classList.contains("active")) id = i
         slides[i].classList.remove("active")
     }
-    
+
     let index = id >= slides.length - 1 ? 0 : id + 1
     slides[index].classList.add("active")
 }
